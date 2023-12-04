55d2cc34ac54d4a77772572f37d98f339d94cc35
PedroMM64 <snupis3@live.com.mx>
2023-12-03 23:16:57 -0600

    Primer avance de la página oficial, solo se tiene la página principal, se sigue desarrollando las otras subpáginas

---
+++ assets/JS/script.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const header = document.getElementById("header");
+    const navbar = document.getElementById("navbar");
+    const menuIcon = document.getElementById("menu-icon");
+    const mobileNav = document.getElementById("mobile-nav");
+    const footerIconTop = document.querySelector(".footericonTop");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 0) {
+            header.classList.add("white-bg");
+        } else {
+            header.classList.remove("white-bg");
+        }
+    });
+
+    menuIcon.addEventListener("click", function () {
+        mobileNav.style.display = (mobileNav.style.display === "flex") ? "none" : "flex";
+    });
+
+    footerIconTop.addEventListener("click", function (event) {
+        event.preventDefault();
+
+        // Desplazarse suavemente hacia la parte superior de la página
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+});
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
